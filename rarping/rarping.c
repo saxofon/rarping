4efcb6a69bbe11e0f4882a6463b74dc014295586
henri.doreau <henri.doreau@d17a7a3c-9449-0410-b21e-9d5bff9bfab3>
2008-04-04 20:41:21 +0000

    still inefficient but it compiles and respects coding style

--- rarping/rarping.c
+++ rarping/rarping.c
@@ -10,11 +10,6 @@
  * $Revision: $
  */
 
-/*
- * Don't panic reading this code, style is as strange as the coding standarts
- * of the project I'm working on are strict! ;)
- */
-
 /* 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,15 +27,16 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>/* malloc() */
+#include <stdlib.h> /* malloc() */
+#include <string.h> /* strncpy(), memcpy() ... */
 #include <unistd.h> /* getopt() : command line parsing */
 #include <errno.h>
 #include <sys/socket.h> /* Network */
 #include <sys/types.h> /* for old systems */
 #include <arpa/inet.h> /* htons() ... */ 
 #include <linux/if_ether.h> /* ETH_P_ALL */
-#include <ioctl.h> /* get infos about net device */
-#include <if.h> /* idem */
+#include <sys/ioctl.h> /* get infos about net device */
+#include <net/if.h> /* idem */
 
 /* Software Version */
 #define VERSION "<none>"
@@ -49,49 +45,57 @@
 /* TODO : this ahs to become an option */
 #define MAX_PROBES 1
 
-/* --- -- --- -- --- -- --- -- --- -- --- -- --- -- --- */
 
-
-extern int optind, opterr, optopt;
+/*
+extern long optind, opterr, optopt;
 extern char *optarg;
+*/
 
-/*
- * RARP packet
- *
- * more : http://www.networksorcery.com/enp/protocol/rarp.htm
- */
+/** @brief this describes a RARP packet */
 typedef struct {
-	unsigned short us_hw_type;
-	unsigned short us_proto_type;
-	unsigned char  uch_hw_len;
-	unsigned char  uch_proto_len;
+	/** @brief Type of hardware */
+	unsigned short us_hwType;
+	/** @brief type of protocol */
+	unsigned short us_protoType;
+	/** @brief length of hardware address in bytes */
+	unsigned char  uc_hwLen;
+	/** @brief length of the protocol address in bytes */
+	unsigned char  uc_protoLen;
+	/** @brief Operation code 3 for a request, 4 for a reply // RFC 903 */
 	unsigned short us_opcode;
-	unsigned char  ucha_src_mac[6];
-	unsigned char  ucha_src_ip[4];
-	unsigned char  ucha_target_mac[6];
-	unsigned char  ucha_target_ip[4];
-} rarp_packet_t;
+	/** @brief source hardware address */
+	char  cht_srcHwAddr[6];
+	/** @brief IP address of the source host */
+	char  cht_srcIpAddr[4];
+	/** @brief hardware address of the remote host */
+	char  cht_targetHwAddr[6];
+	/** @brief IP address of the remote host */
+	char  cht_targetIpAddr[4];
+} rarpPacket_t;
 
-/*
- * Full Ethernet Trame (IPV4 RARP packet)
- */
+/** @brief full ethernet trame, MAC headers + RARP packet as described above */
 typedef struct {
-	unsigned char  ucha_dest_mac[6];
-	unsigned char  ucha_sender_mac[6];
-	unsigned short us_eth_type;
-	rarp_packet_t  str_packet;
-} ether_packet_t;
+	/** @brief Harware address of the device we send to */
+	unsigned char  ucht_destHwAddr[6];
+	/** @brief Hardware address we of the device we send from */
+	unsigned char  ucht_senderHwAddr[6];
+	/** @brief ethertype (0x8035 for RARP)*/
+	unsigned short us_ethType;
+	/** @brief RARP packet
+	 * @see rarpPacket_t
+	 */
+	rarpPacket_t  str_packet;
+} etherPacket_t;
 
 
-/***********************************************************************************************************************************/
-
-/* Contains options/informations given trought command line */
+/** @brief Contains options/informations given trought command line */
 typedef struct {
-	char * pch_iface; /* Interface to use to send probes */
-	char * pch_dest_mac_addr; /* destination MAC address to craft packet */
+	/** @brief choosen Interface */
+	char * pch_iface;
+	/** @brief Subject of the request(s) */
+	char * pch_askedHwAddr;
 } opt_t;
 
-/* Ethernet header */
 
 /* ******************************************************************************************************************************* */
 
@@ -105,92 +109,100 @@ void usage ( void );
  *
  * @see opt
  */
-signed char argument_management ( long l_argc, char **ppch_argv, opt_t *pst_args_dest );
+signed char argumentManagement ( long l_argc, char **ppch_argv, opt_t *pst_argsDest );
 
 /**
  * @brief perform RARP requests the way defined by user
  */
-signed char perform_requests ( const opt_t *pst_args_dest );
+signed char performRequests ( const opt_t *pst_argsDest );
+
+/**
+ * @brief find out and packs the datas we'll send
+ */
+signed char craftPacket ( etherPacket_t * pstr_packet, char * pch_ifaceName, long l_socket );
+
 
 /* ******************************************************************************************************************************* */
 /* ******************************************************************************************************************************* */
 
 
-int main ( int l_argc, char **ppch_argv )
+int main ( int i_argc, char **ppch_argv )
 {
     /* Return value */
-    signed char ch_ret_value;
+    signed char c_retValue;
+	long l_argc;
 	opt_t str_args;
 
-    ch_ret_value = 0;
+	l_argc = i_argc; /* if sizeof(int) != sizeof(long) */
+    c_retValue = 0;
 
     /* Parse args using getopt to fill a struct (args), return < 0 if problem encountered */
-    if ( argument_management(l_argc, ppch_argv, &str_args) > 0 )
+    if ( argumentManagement(l_argc, ppch_argv, &str_args) > 0 )
     {
         /* perform RARP requests as wanted by user (using args struct) */
-        ch_ret_value = perform_requests(&str_args);
+        c_retValue = performRequests(&str_args);
     }
     else
     {
         /* If any problem occured, explain the user how to build his command line */
         usage();
         /* and exit < 0 */
-        ch_ret_value = -1;
+        c_retValue = -1;
     }
 
 
     /* Simple exit point */
-    return ch_ret_value;
+    return c_retValue;
 }
 
 
-signed char argument_management ( long l_argc, char **ppch_argv, opt_t *pst_args_dest )
+signed char argumentManagement ( long l_argc, char **ppch_argv, opt_t *pst_argsDest )
 {
-	char ch_ret_value;
+	signed char c_retValue;
 	char ch_opt;
 
 	/* Initialisation */
-	ch_ret_value = 1;
-	pst_args_dest->pch_iface = NULL;
-	pst_args_dest->pch_dest_mac_addr = NULL;
+	c_retValue = 1;
+	pst_argsDest->pch_iface = NULL;
+	pst_argsDest->pch_askedHwAddr = NULL;
 	/* ************** */
 
 
 	/* Parsing options args */
-	while ( ( ch_opt = getopt( l_argc, ppc_argv, "I:" ) ) != -1 ) 
+	while ( ( ch_opt = getopt( l_argc, ppch_argv, "I:" ) ) != -1 ) 
 	{
 		switch(ch_opt)
 		{
-			case 'I'	:	pst_args_dest->pc_iface = optarg;
+			case 'I'	:	pst_argsDest->pch_iface = optarg;
 							break;
 
 			case 'h'	:
 			case '?'	:
-			default		:	ch_ret_value = -1;
+			default		:	c_retValue = -1;
 		}
 	}
 	
 	/* parsing non options args */
 	/* The only one must be the MAC Addr we'll request related IP */
 	if (optind < l_argc)
-		pst_args_dest->pc_dest_mac_addr = ppch_argv[optind];
+		pst_argsDest->pch_askedHwAddr = ppch_argv[optind];
 	else
-		c_ret_value = -1;
+		c_retValue = -1;
 
 	/* Check if required infos had been given */
-	if ( (pst_args_dest->pc_iface == NULL) || (pst_args_dest->pc_dest_mac_addr == NULL) )
-		c_ret_value = -1;
+	if ( (pst_argsDest->pch_iface == NULL) || (pst_argsDest->pch_askedHwAddr == NULL) )
+		c_retValue = -1;
 	else
 	{
 		/* nothing to do */
 	}
 
 #ifdef DEBUG
-	fprintf(stderr, "Iface : %s\n", pst_args_dest->pc_iface);
-	fprintf(stderr, "Iface : %s\n", pst_args_dest->pc_dest_mac_addr);
+	fprintf(stderr, "Iface : %s\n", pst_argsDest->pch_iface);
+	fprintf(stderr, "Request about : %s\n", pst_argsDest->pch_askedHwAddr);
 #endif
 
-	return c_ret_value;
+	return c_retValue;
 }
 
 
@@ -198,7 +210,6 @@ void usage ( void )
 {
 	printf("\n-=] Rarp requests sender [=-\n");
 	printf("           ****          \n");
-	printf("\nhttp://hatch-the-hitch.blogspot.com\n");
 	printf("\nVersion : %s\n", VERSION);
 	printf("Usage : ./rarping -I [interface] [request MAC address]\n");
 	printf("For example : ./rarping -I eth0 00:03:13:37:be:ef\n\n");
@@ -207,63 +218,72 @@ void usage ( void )
 }
 
 
-signed char perform_requests ( const opt_t *pst_args_dest )
+signed char performRequests ( const opt_t *pst_argsDest )
 {
-	long l_socket, nb_probes;
-	char ch_ret_value;
-	ether_packet_t str_packet;
+	long l_socket, nbProbes;
+	signed char c_retValue;
+	etherPacket_t str_packet;
 
-	ch_ret_value = 1;
-	nb_probes = 0;
+	c_retValue = 1;
+	nbProbes = 0;
 
 	if ((l_socket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) /* socket opening */
 	{
 		perror("socket");
-		return -1;
+		if (!IS_ROOT)
+			fprintf(stderr, "Are you root?\n");
+		c_retValue = -1;
 	}
 	else
 	{
-		/* nothing to do */
-	}
-
-	if (craft_packet(&str_packet, pst_args_dest->pch_iface))
-		while (++nb_probes < MAX_PROBES)
-			if (send_probe(l_socket, &str_packet) != 1)
-			{
-				fprintf(stderr, "Can't send request #%d\n", nb_probe);
-			}
-			else
-			{
+		if (craftPacket(&str_packet, pst_argsDest->pch_iface, l_socket))
+			while (++nbProbes < MAX_PROBES);
+/*				if (send_probe(l_socket, &str_packet) != 1)
+				{
+					fprintf(stderr, "Can't send request #%d\n", nbProbes);
+				}
+				else
+				{
 #ifdef DEBUG
-				fprintf(stdout, "Request #%d sent\n", nb_probe);
+					fprintf(stderr, "Request #%d sent\n", nbProbes);
 #endif
-			}
-	else
-	{
-		fprintf(stderr, "Ouuch!!\nWill now abort!\n");
-		exit(EXIT_FAILURE);
+				}*/
+		else
+		{
+			fprintf(stderr, "Can't craft packets\n");
+			exit(EXIT_FAILURE);
+		}
+	
+		close(l_socket);
 	}
 	
-	close(l_socket);
-	
-	return ch_ret_value;
+	return c_retValue;
 }
 
 
-signed char craft_packet ( ether_packet_t * pstr_packet, char * pch_iface_name, long l_socket )
+signed char craftPacket ( etherPacket_t * pstr_packet, char * pch_ifaceName, long l_socket )
 {
+	signed char c_retValue;
 	struct ifreq str_device; /* described in man (7) netdevice */
 
-	strncpy(str_device.ifr_name, pch_iface_name, IFNAMESIZ);
+	c_retValue = 0;
 
+	strncpy(str_device.ifr_name, pch_ifaceName, IFNAMSIZ); /* copy iface name into the ifreq structure we've just declared // IFNAMSIZ defined in net/if.h */
+
+	/* Get local hardware address */
 	if (ioctl(l_socket, SIOCGIFHWADDR, &str_device) == -1 )
 	{
 		perror("ioctl");
-		exit(EXIT_FAILURE);
+		c_retValue = -1;
 	}
 	else
 	{
-		/* nothing to do */
+#ifdef DEBUG
+#define MAC(i) (str_device.ifr_hwaddr.sa_data[(i)])
+		fprintf(stderr, "Local MAC address is %2x:%2x:%2x:%2x:%2x:%2x\n", MAC(0), MAC(1), MAC(2), MAC(3), MAC(4), MAC(5));
+#endif
 	}
+
+	return c_retValue;
 }
 
